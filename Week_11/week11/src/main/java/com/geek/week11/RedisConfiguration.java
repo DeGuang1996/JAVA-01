f14b0250288a01ec4422006d3fdfabc6b9877c3b
deguang <1969117214@qq.com>
2021-05-11 23:19:32 +0800

    第11周作业提交

---
+++ Week_11/week11/src/main/java/com/geek/week11/RedisConfiguration.java
@@ -0,0 +1,19 @@
+package com.geek.week11;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Bean(name = "redisson", destroyMethod="shutdown")
+    RedissonClient redisson() {
+        //1、创建配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
+    }
+}
---
+++ Week_11/week11/src/main/java/com/geek/week11/annotation/DistributedLock.java
@@ -0,0 +1,22 @@
+package com.geek.week11.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface DistributedLock {
+
+    /**
+     * 锁的名称
+     */
+    String value() default "redisson";
+
+    /**
+     * 锁的有效时间
+     */
+    int leaseTime() default 10;
+}
+
+
